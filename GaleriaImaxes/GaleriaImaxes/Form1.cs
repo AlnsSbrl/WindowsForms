cc37c481edde4db1b0b1f38a08c4d0b8718fc74a
alnssbrl <alonsosobralpablo@gmail.com>
2022-11-16 10:24:20 +0100

    rematado

---
+++ GaleriaImaxes/GaleriaImaxes/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleriaImaxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Visor de imágenes";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            //openFileDialog.ShowDialog();
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = "C:\\Users\\Pablo\\Desktop\\imagenes";
+                ofd.Filter = "PNG files (*.png)|*.png |JPG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+                ofd.FilterIndex = 4;
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    String filepath=ofd.FileName;
+                    if (checkBoxModal.Checked)
+                    {
+                        FormImaxes f = new FormImaxes(filepath);                       
+                        f.ShowDialog();
+                    }
+                    else
+                    {
+                        FormImaxes f = new FormImaxes(filepath);
+                        f.Show();
+                    }
+
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ GaleriaImaxes/GaleriaImaxes/FormImaxes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleriaImaxes
+{
+    public partial class FormImaxes : Form
+    {
+        public FormImaxes(String filepath)
+        {
+            InitializeComponent();
+            Size = Image.FromFile(filepath).Size;
+            pictureBox1.Image= Image.FromFile(filepath);
+            FileInfo file = new FileInfo(filepath);
+            Text = file.Name;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            Bounds = pictureBox1.Bounds;
+        }
+                        
+        private void FormImaxes_Resize(object sender, EventArgs e)
+        {
+            pictureBox1.Size = Size;
+        }
+    }
+}
